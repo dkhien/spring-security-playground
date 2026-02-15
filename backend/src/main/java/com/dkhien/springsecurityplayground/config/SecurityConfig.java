b3433204be66cbb86857ac7a62d1fdf7ee50ba43
Hien Doan <dkhien.work@gmail.com>
2026-02-15 16:58:41 +0700

    feat: basic authentication with hardcoded users

--- backend/src/main/java/com/dkhien/springsecurityplayground/config/SecurityConfig.java
+++ backend/src/main/java/com/dkhien/springsecurityplayground/config/SecurityConfig.java
@@ -1,8 +1,50 @@
 package com.dkhien.springsecurityplayground.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/api/public").permitAll()
+                .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        )
+        .httpBasic(Customizer.withDefaults());
 
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        String password = encoder.encode("password");
+        UserDetails normalUser = User.withUsername("user").password(password).roles("USER").build();
+        UserDetails adminUser = User.withUsername("admin")
+                .password(encoder.encode("password"))
+                .roles("ADMIN")
+                .build();
+        List<UserDetails> userDetails = new ArrayList<>();
+        userDetails.add(normalUser);
+        userDetails.add(adminUser);
+        return new InMemoryUserDetailsManager(userDetails);
+    }
 }
