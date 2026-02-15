0e25b97a2e7c315ed8cfcfefadc00caa8bd08e6b
Hien Doan <dkhien.work@gmail.com>
2026-02-15 22:02:30 +0700

    fix: config AuthenticationManager and remove comment from application.properties

--- backend/src/main/java/com/dkhien/springsecurityplayground/config/SecurityConfig.java
+++ backend/src/main/java/com/dkhien/springsecurityplayground/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.dkhien.springsecurityplayground.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
@@ -30,6 +32,11 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
