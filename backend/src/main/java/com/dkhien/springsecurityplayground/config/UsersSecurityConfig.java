5dfb852dfb0ef8ee8e0f75cea6b411469520cd3e
Hien Doan <dkhien.work@gmail.com>
2026-02-16 20:53:59 +0700

    fix: add all auth methods into UsersSecurityConfig chain

--- backend/src/main/java/com/dkhien/springsecurityplayground/config/UsersSecurityConfig.java
+++ backend/src/main/java/com/dkhien/springsecurityplayground/config/UsersSecurityConfig.java
@@ -1,19 +1,28 @@
 package com.dkhien.springsecurityplayground.config;
 
+import com.dkhien.springsecurityplayground.security.JwtAuthenticationFilter;
 import com.dkhien.springsecurityplayground.security.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class UsersSecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public UsersSecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
     @Bean
     @Order(4)
-    SecurityFilterChain usersFilterChain(HttpSecurity http) {
+    SecurityFilterChain usersFilterChain(HttpSecurity http) throws Exception {
         http.securityMatcher("/api/users/**")
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers(HttpMethod.GET, "/api/users").hasRole(Role.ADMIN.name())
@@ -21,6 +30,8 @@ public class UsersSecurityConfig {
                         .requestMatchers(HttpMethod.PUT, "/api/users/{id}/role").hasRole(Role.ADMIN.name())
                         .anyRequest().authenticated()
                 )
+                .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .csrf(csrf -> csrf.disable());
 
         return http.build();
